8cb67eceaf3c4262c6badaf9df94fdc5c6e8d291
dell <dell>
2016-08-12 08:51:31 +0800

    张三

---
+++ src/com/zse/Student.java
@@ -0,0 +1,27 @@
+package com.zse;
+
+public class Student {
+  private int sid;
+  private String pass;
+  private String name;
+public int getSid() {
+	return sid;
+}
+public void setSid(int sid) {
+	this.sid = sid;
+}
+public String getPass() {
+	return pass;
+}
+public void setPass(String pass) {
+	this.pass = pass;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+  
+  
+}
